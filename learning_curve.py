ebff2a866bbab0d9b89e81ff5588da868fb706f7
Alisha P <apegan@wellesley.edu>
2017-03-12 21:45:51 -0400

    Turning in Machine Learning toolbox

--- learning_curve.py
+++ learning_curve.py
@@ -20,18 +20,22 @@ def display_digits():
 
 def train_model():
     data = load_digits()
-    num_trials = 10
+    num_trials = 90
     train_percentages = range(5, 95, 5)
     test_accuracies = numpy.zeros(len(train_percentages))
-
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
-
-    # TODO: your code here
+    for i in range(len(train_percentages)):
+        accuracies = []
+        for j in range(num_trials):
+            size = train_percentages[i] / 100
+            X_train, X_test, y_train, y_test = train_test_split(data.data,
+                                                                data.target,
+                                                                train_size=size)
+            model = LogisticRegression(C=10**-1)
+            model.fit(X_train, y_train)
+            accuracies.append(model.score(X_test, y_test))
+            # print("Train accuracy %f" % model.score(X_train, y_train))
+            # print("Test accuracy %f" % model.score(X_test, y_test))
+        test_accuracies[i] = numpy.mean(accuracies)
 
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
@@ -41,6 +45,5 @@ def train_model():
 
 
 if __name__ == "__main__":
-    # Feel free to comment/uncomment as needed
     display_digits()
-    # train_model()
+    train_model()
